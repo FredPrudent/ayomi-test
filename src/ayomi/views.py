d552e67939ba8dc9c3cfbc1d33317432bc8658b4
FredPrudent <fredericbourgain@yahoo.com>
2019-12-12 19:45:19 +0100

    Mise en place du formulaire d'inscription sur la page Home

--- src/ayomi/views.py
+++ src/ayomi/views.py
@@ -1,6 +1,29 @@
+# coding: utf-8
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import SignUpForm
 
 def home (request):
-    return render(request, "home.html", {})
+    title = "Bienvenue"
+    
+    form = SignUpForm(request.POST or None)
+
+    if request.user.is_authenticated():
+        title = "Bienvenue %s" %(request.user)
+
+    context = {
+        "title": title,
+        "form" : form
+    }
+
+    
+
+    if form.is_valid():
+        instance = form.save (commit=False)
+        instance.save()
+        context = {
+            "title" : "Vous vous êtes bien enregistré"
+        }
+
+    
+    return render(request, "home.html", context)
 
