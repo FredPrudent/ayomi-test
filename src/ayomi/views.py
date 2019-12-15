c64c233f423b506d0c8770a673e9330366a18f6e
FredPrudent <fredericbourgain@yahoo.com>
2019-12-15 22:44:23 +0100

    Update de l'email de l'user via la modale

--- src/ayomi/views.py
+++ src/ayomi/views.py
@@ -3,7 +3,7 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from ayomi.forms import SignUpForm
+from .forms import SignUpForm, UserUpdateForm
 from django.contrib.auth.decorators import login_required
 
 def home(request):
@@ -17,10 +17,20 @@ def home(request):
             
             login(request, user)
             return redirect('profile')
+    
     else:
         form = SignUpForm()
     return render(request, 'home.html', {'form': form})
 
 @login_required
 def profile (request):
-    return render (request, 'profile.html')
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+        return render (request, 'profile.html', {'form': form})
+
+
+    else:
+        form= UserUpdateForm(instance=request.user)
+    return render (request, 'profile.html', {'form': form})
